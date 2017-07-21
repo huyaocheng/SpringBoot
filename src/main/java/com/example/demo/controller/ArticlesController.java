d075e00beac909f4b0a0455d6202e62deae9e6dd
huyaocheng <huyaocheng@58qf.com>
2017-07-22 00:26:46 +0800

    ceshi

--- src/main/java/com/example/demo/controller/ArticlesController.java
+++ src/main/java/com/example/demo/controller/ArticlesController.java
@@ -26,7 +26,7 @@ public class ArticlesController {
     @RequestMapping("hello")
     private String getHellowWorld(){
 
-        return "hello-world my git and jenkins is work";
+        return "hello-world ";
     }
 
     @RequestMapping("get")
