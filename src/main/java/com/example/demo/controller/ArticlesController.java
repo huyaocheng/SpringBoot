10ebdb907b27e79b5dc4e1285b64ef5c594b0a01
huyaocheng <huyaocheng@58qf.com>
2017-07-22 13:07:23 +0800

    测试git提交

--- src/main/java/com/example/demo/controller/ArticlesController.java
+++ src/main/java/com/example/demo/controller/ArticlesController.java
@@ -26,7 +26,7 @@ public class ArticlesController {
     @RequestMapping("hello")
     private String getHellowWorld(){
 
-        return "hello-world jdfjsdfiu s f";
+        return "hello-world eerteddf";
     }
 
     @RequestMapping("get")
