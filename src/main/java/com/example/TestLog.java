d7fa61c33b7bb0f88049b7ea50fe253b9e80ec32
huyaocheng <huyaocheng@58qf.com>
2017-07-26 17:12:06 +0800

    测试git提交

--- src/main/java/com/example/TestLog.java
+++ src/main/java/com/example/TestLog.java
@@ -10,9 +10,13 @@ public class TestLog {
     private static final Logger logger = LoggerFactory.getLogger(TestLog.class);
 
     public static void main(String[] args) {
+
         logger.debug("This is a debug message!");
+
         logger.info("This is info message!");
+
         logger.warn("This is a warn message!");
+
         logger.error("This is error message!");
     }
 }
